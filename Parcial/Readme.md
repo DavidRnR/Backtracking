47c4a006d799de83e595b79e2d68e2fd13ee3a62
David Martín <carlosdavidmartin@gmail.com>
2017-06-18 21:50:48 -0300

    Parcial TUDAI2017

---
+++ Parcial/Readme.md
@@ -0,0 +1,10 @@
+# Programación III - TUDAI 2017
+
+## Backtracking Parcial
+
+Escriba en lenguaje JAVA un algoritmo utilizando la técnica de Backtracking que resuelva el siguiente problema:
+
+Dado un conjunto de n números enteros se desea encontrar, si existe, una partición en tres subconjuntos disjuntos, tal que la suma de sus elementos sea la misma.
+Por ejemplo, dado el conjunto {4,1,2,6,3,5,9}, existe una partición en tres conjuntos {4,6} {5,3,2} {9,1} tal que cada uno de los tres conjuntos suma el mismo valor, 10.
+
+## Martín, Carlos David
---
+++ Parcial/src/BacktrackingArrayList.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class BacktrackingArrayList {
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+	public BacktrackingArrayList () {
+		numbers.add(4);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(6);
+		numbers.add(3);
+		numbers.add(5);
+		numbers.add(9);
+
+		ArrayList<Integer> group = new ArrayList<Integer>();
+		group.add(0);
+		group.add(0);
+		group.add(0);
+		group.add(0);
+		group.add(0);
+		group.add(0);
+		group.add(0);
+
+		getSolution(numbers, group, 0);
+	}
+
+	public void getSolution (ArrayList<Integer> numb, ArrayList<Integer> group, int level) {
+
+		if(level == numb.size()) {
+			if(isSolution(numb,group)){
+				System.out.println(numb + " "+group);
+				printSol(numb,group);
+			}
+		}
+		else {
+			for (int i = 0; i < 3; i++) {
+				group.set(level, i);
+				getSolution(new ArrayList<Integer> (numb), new ArrayList<Integer>(group) , level+1);
+
+			}
+		}
+
+	}
+
+	private boolean isSolution(ArrayList<Integer> numb,ArrayList<Integer> group) {
+		Integer plus1 = 0;
+		Integer plus2 = 0;
+		Integer plus3 = 0;
+
+		for (int i = 0; i < group.size(); i++) {
+			if(group.get(i) == 0) {
+				plus1 += numb.get(i);
+			}
+			else if(group.get(i) == 1) {
+				plus2 += numb.get(i);
+			}
+			else if (group.get(i) == 2) {
+				plus3 += numb.get(i);
+			}
+
+		}
+		return plus1 == plus2 && plus2 == plus3 && plus1 == plus3;
+	}
+
+	private void printSol(ArrayList<Integer> numb, ArrayList<Integer> group) {
+		System.out.println("SOLUCION ENCONTRADA!:");
+		for (int i = 0; i < group.size(); i++) {
+			System.out.println("Elemento " + numb.get(i) + ": Pertenece al conjunto Conj: " + group.get(i));
+		}
+
+		System.out.println();
+		System.out.println();
+
+	}
+
+
+	public static void main(String[] args) {
+		BacktrackingArrayList b = new BacktrackingArrayList();
+	}
+}
+
